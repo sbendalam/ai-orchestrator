0c7c26d5927d492770e002f33b0336d9998bf248
Sai Krishna Bendalam <saibendalam@people.health>
2024-02-17 10:46:54 +0530

    Intial commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,53 +1,47 @@
 require("dotenv").config();
+const sql = require('mssql')
 const express = require("express")
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const apiRouter = require("./routes/apiRouter");
 
-// Initializing function creates an Express application.
 const app = express();
 
-//Middleware to parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(express.json())
 
-//Configuring express to serve frontend
 app.use(express.static(path.resolve(__dirname, "../../publish")));
 
-//Initializing CORS Options change before prod to accept necessary origins only
 app.use((req, res, next) => {
-    // Website you wish to allow to connect
     res.setHeader("Access-Control-Allow-Origin", "*");
-    // Request methods you wish to allow
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
     );
-    // Request headers you wish to allow
     res.setHeader(
       "Access-Control-Allow-Headers",
       "X-Requested-With,content-type"
     );
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader("Access-Control-Allow-Credentials", true);
-    // Pass to next layer of middleware
     next();
   });
 
-//routing all /ai routes to main
 app.use("/api", apiRouter);
 
-//Redirecting all remaining requests to frontend
 app.get("*", function (req, res) {
 
   res.sendFile('index.html',{root: path.resolve(__dirname, "../../publish")});
 });
 
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
-connection.end()
+const config = {
+  server : process.env.SERVER,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database:process.env.DATABASE
+}
+
+const pool = sql.connect(config).then(() => {
+  console.log("DB Connected")
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
--- server/src/routes/apiRouter.js
+++ server/src/routes/apiRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const testController = require("../controllers/testController");
+const testController = require("../controllers/taskAssign.Controller");
 
 const apiRouter = express.Router();
 
-apiRouter.get("/test", testController.getFromTest);
-apiRouter.post("/test", testController.postEcho);
+// apiRouter.get("/test", testController.getFromTest);
+// apiRouter.post("/test", testController.postEcho);
 
 module.exports = apiRouter;
